c71afce00967a9b80bee2dd7e7724d9f6ead456c
FikriSatria11 <muhfikrisatria@gmail.com>
2019-08-28 15:53:53 +0800

    move the player

--- game.py
+++ game.py
@@ -7,6 +7,14 @@ pygame.init()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
 
+# key mapping
+keys = {
+    "top": False,
+    "bottom": False,
+    "left": False,
+    "right": False
+}
+
 running = True
 
 playerpos = [100, 100] #inisialisasi posisi pemain
@@ -46,3 +54,34 @@ while(running):
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+
+        # check keydown dan keyup
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_w:
+                keys["top"] = True
+            elif event.key == K_a:
+                keys["left"] = True
+            elif event.key == K_s:
+                keys["bottom"] = True
+            elif event.key == K_d:
+                keys["right"] = True
+        if event.type == pygame.KEYUP:
+            if event.key == K_w:
+                keys["top"] = False
+            elif event.key == K_a:
+                keys["left"] = False
+            elif event.key == K_s:
+                keys["bottom"] = False
+            elif event.key == K_d:
+                keys["right"] = False
+    # end of event loop
+
+    #9. perpindahan pemain
+    if keys["top"]:
+        playerpos[1] -= 5 # kurangi nilai y
+    elif keys["bottom"]:
+        playerpos[1] += 5 # tambah nilai y
+    if keys["left"]:
+        playerpos[0] -= 5 # kurangi nilai x
+    elif keys["right"]:
+        playerpos[0] += 5 # tambah nilai x
